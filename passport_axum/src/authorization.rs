2689243e78e364c2d7bb5bb8f41ded0338a64ae8
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-01-25 02:04:05 +0100

    (axum): authorization middleware fn

---
+++ passport_axum/src/authorization.rs
@@ -0,0 +1,58 @@
+use axum::body::Body;
+use axum::extract::Request;
+use axum::http::StatusCode;
+use axum::middleware::Next;
+use axum::response::{IntoResponse, Response};
+use axum::{http, Json};
+use passport_core::decoder::DecodeAccessToken;
+use serde_json::json;
+
+pub struct AuthError {
+    message: String,
+    status_code: StatusCode,
+}
+
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        let body = Json(json!({
+            "message": self.message
+        }));
+
+        (self.status_code, body).into_response()
+    }
+}
+
+pub async fn authorization_middleware(
+    mut req: Request,
+    next: Next,
+    decoder: Box<dyn DecodeAccessToken>,
+) -> Result<Response<Body>, AuthError> {
+    let header_value = req.headers_mut().get(http::header::AUTHORIZATION);
+
+    let auth_header = match header_value {
+        None => Err(AuthError {
+            message: "Please add the JWT token to the header".into(),
+            status_code: StatusCode::FORBIDDEN,
+        }),
+        Some(header) => header.to_str().map_err(|_| AuthError {
+            message: "Empty header is not allowed".into(),
+            status_code: StatusCode::FORBIDDEN,
+        }),
+    }?;
+
+    let mut header = auth_header.split_whitespace();
+
+    let (bearer, token) = (header.next(), header.next());
+
+    let claims_principal = match decoder.decode_access_token(token.unwrap().into()).await {
+        Ok(authentication) => authentication,
+        Err(err) => Err(AuthError {
+            message: err.to_string(),
+            status_code: StatusCode::FORBIDDEN,
+        })?,
+    };
+
+    req.extensions_mut().insert(claims_principal);
+
+    Ok(next.run(req).await)
+}
